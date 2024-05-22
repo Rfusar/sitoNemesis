ba2b6b3546199a2165e01ddbd334b6609e55f56c
Rfusar <rfusaro12@gmail.com>
2024-05-22 03:23:45 +0200

    0.0.0

--- static/js/event.js
+++ static/js/event.js
@@ -1,13 +1,13 @@
 
 let check = 0
+let text = null
 window.addEventListener("scroll", ()=>{
   const w = window.scrollY
-  let text = null
   const DIV = divLeft
   //console.log(w)
   if (w < 100){    
     DIV.innerHTML = ""
-    text = create("h1", null, null, {fontSize: "60px"})
+    text = create("h1", null, null, {fontSize: "60px", transition: ".5s"})
     text.textContent = testo1
     DIV.append(text)
     text.style.opacity = 1
@@ -15,16 +15,23 @@ window.addEventListener("scroll", ()=>{
     logo.style.opacity = 1
     nav.style.opacity = 1
     footer.style.opacity = 1
-
   }
-  else if (w >= 100 && w < 130){
+  else if (w >= 100 && w < 120){
     text.style.opacity = 0
   }
-  else if(w >= 130 && w < 300){
+  else if (w >= 120 && w < 220){
     DIV.innerHTML = ""
-    text = create("h1", null, null, {fontSize: "60px"})
+    const divRight = create("section", "divRight")
+    text = create("h1", null, null, {fontSize: "60px", transition: ".5s"})
     text.textContent = testo2 
+    text.style.opacity = 0
     DIV.append(text)
+  }
+  else if (w >= 220 && w < 250){
+    text.style.opacity= 0
+  }
+  else if(w >= 250 && w < 350){
+    text.style.opacity = 1
     DIV.style.width = "50%"
 
     logo.style.opacity = 0
@@ -34,7 +41,7 @@ window.addEventListener("scroll", ()=>{
     check = 0
   }
 
-  else if (w > 300){
+  else if (w >= 350 && w < 450){
      if(check == 0){
         check++
         DIV.style.width = "100%"
@@ -53,11 +60,6 @@ window.addEventListener("scroll", ()=>{
         
         }
      }
-  }
-  else{
-    logo.style.opacity = 1
-    nav.style.opacity = 1
-    footer.style.opacity = 1
   }
 
 })
--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ const nav = create("nav", "navbar")
 const logo = create("img", null, "logoSection1")
 logo.src = "https://www.nemesisag.com/images/nemesis-solution-white.svg"
 
-const e = create("p", "itemNavbar")
+const e = create("p", "itemNavbar", null, {marginLeft: "60px"})
 e.innerHTML = `Premi 
 <img src='static/img/shift.png' style="width: 40px; height: 40px"/> + 
 <img src='static/img/s.png' style="width: 40px; height: 40px"/> 
@@ -16,8 +16,10 @@ const titleFirstSection = create("h1", null, null, {
   fontSize: "60px",
   transition: ".5s"
 })
+
 titleFirstSection.textContent= "Soluzioni digitali per i servizi e il commercio"
 divLeft.append(titleFirstSection)
+
 
 
 const footer = create("footer", "footer")
@@ -29,6 +31,6 @@ footer_p.innerHTML = `
 `
 footer.append(footer_p)
 
-for(const e of new Array(nav, footer, logo, divLeft)){
+for(const e of new Array(nav, footer, logo, divLeft, divRight)){
   document.querySelector(".vignette-radial").append(e)
 }
