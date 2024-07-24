b9ca601115e7a7dba92c3efa4f2e34cd3ca38264
Rfusar <rfusaro12@gmail.com>
2024-07-25 01:34:23 +0200

    0.0.0

--- src/componenti/navbar/init.jsx
+++ src/componenti/navbar/init.jsx
@@ -3,18 +3,24 @@ import './navbar.css'
 
 export default function Navbar(){
     return (
-        <nav id="navbar">
-            <div>
-                <img src="/logoNemesis.svg" alt=""/>
-                <span>Prodotti</span>
-                <span>Consuleza</span>
-                <a href="#section4">Contatti</a>
-            </div>
+        <section id="cardNavbar">
+            <nav id="navbar">
+                <div>
+                    <img src="/logoNemesis.svg" alt=""/>
+                    <span>Prodotti</span>
+                    <span>Consuleza</span>
+                    <a href="#section4">Contatti</a>
+                </div>
             
-            <div>
-                <Link to="/"></Link>    
-                <Link to="/"></Link>    
+                <div>
+                    <Link to="/"></Link>    
+                    <Link to="/"></Link>    
+                </div>
+
+            </nav>
+            <div style={{height: "0px", backgroundColor: "#fff"}}>
+                
             </div>
-        </nav>
+        </section>
     )
 }
