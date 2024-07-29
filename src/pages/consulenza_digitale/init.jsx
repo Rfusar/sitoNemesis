b51a758cc35c98406dd21ff51d8b5470da7ad540
Rfusar <rfusaro12@gmail.com>
2024-07-29 02:57:55 +0200

    0.0.0

--- src/pages/consulenza_digitale/init.jsx
+++ src/pages/consulenza_digitale/init.jsx
@@ -1,10 +1,12 @@
 import Section_uno from './section1.jsx'
+import Section_due from './section2.jsx'
 
 
 export default function Init_consulenza_digitale(){
     return (
         <section>
             <Section_uno/>
+            <Section_due/>
         </section>
     )
 }
---
+++ src/pages/consulenza_digitale/section2.jsx
@@ -0,0 +1,30 @@
+export default function Section_due(){
+    return (
+        <section id="cd_section2">
+            <div id="cd_vantaggi">
+                <div>
+                    <p>
+                        Gestisci in modo più profittevole i tuoi affari, sviluppando innovazione tecnologica e di processo. 
+                        Crea un ambiente di lavoro collaborativo e coinvolgente, e permetti a dipendenti, management e 
+                        clienti di utilizzare servizi e funzioni aziendali ovunque si trovino, e con qualsiasi dispositivo.
+                        Rafforza la tua azienda, implementa soluzioni digitali per il tuo business
+                    </p>
+                </div>
+                <div id="cd_card2Section2">
+                    <div>
+                        <div className="imgOK"><img src="/src/assets/ok.png" height="50" alt=""/></div>
+                        <p>ABBATTI TEMPI E COSTI</p>
+                    </div>
+                    <div>
+                        <div className="imgOK"><img src="/src/assets/ok.png" height="50" alt=""/></div>
+                        <p>SEMPLIFICA LE PROCEDURE</p>
+                    </div>
+                    <div>
+                        <div className="imgOK"><img src="/src/assets/ok.png" height="50" alt=""/></div>
+                        <p>MIGLIORA LE PERFORMANCE</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
